fc5d5274435b419129b51440ab38bac50ccb655d
edwardma401 <64027921+edwardma401@users.noreply.github.com>
2020-06-08 15:44:53 -0400

    finished DOB part

--- hmm.py
+++ hmm.py
@@ -6,6 +6,7 @@ csv_path = os.path.join("Resources", "em.csv")
 First = []
 Last = []
 Emp = []
+DOB = []
 with open (csv_path) as reading_obj:
     csv_reader = csv.reader(reading_obj)
     next(csv_reader)
@@ -17,17 +18,31 @@ with open (csv_path) as reading_obj:
         Last.append(Last_Name)
         emp = row[0]
         Emp.append(emp)
+        dob = row[2]
+        DOB.append(dob)
+
+year = [i.split('-')[0] for i in DOB]
+# print(year)
+month = [i.split('-')[1] for i in DOB]
+# print(month)
+days = [i.split('-')[2] for i in DOB]
+# print(days)
+mdy = []
+qcount = 0
+for i in year:
+    x = str(month[qcount]) + '/' + str(days[qcount]) + '/' + str(year[qcount])
+    mdy.append(x)
+    qcount += 1
+    if qcount == len(year):
+        break
+# print(mdy)
 
 with open ("csvpractice1.csv", 'w', newline='') as practicefile:
-    thiswriter = csv.DictWriter(practicefile, ["EMP ID", "First Name", "Last Name"])
+    thiswriter = csv.DictWriter(practicefile, ["EMP ID", "First Name", "Last Name", "DOB"])
     thiswriter.writeheader()
     rcount = 0
-    scount = 0
-    ecount = 0
     for row in First:
-        thiswriter.writerow({"EMP ID": Emp[ecount], "First Name": First[rcount], "Last Name": Last[scount]})
+        thiswriter.writerow({"EMP ID": Emp[rcount], "First Name": First[rcount], "Last Name": Last[rcount], "DOB": mdy[rcount]})
         rcount += 1
-        scount += 1
-        ecount += 1
         if rcount == len(First):
             break
